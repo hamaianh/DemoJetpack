da6331c4d3c4a5ec57b334c09f124c2c627b2293
Mai Anh Ha <anhhm@inspirelab.io>
2021-08-19 14:35:29 +0700

    refs #2 Basic

--- app/src/main/java/com/example/demojetpack/MainActivity.kt
+++ app/src/main/java/com/example/demojetpack/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.demojetpack
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,10 +28,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //jetback
         setContent {
-            JetpackComposeTheme{
+            JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MessageCard("Mai Anh Ha", "Chào mừng các bạn đến với demo jetpack")
                 }
             }
         }
@@ -27,14 +39,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String){
-    Text(text = "Hello $name")
+fun MessageCard(name: String, description: String) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "image description",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape),
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = "Hello $name",
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(4.dp)
+                )
+            }
+
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Light Mode")
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun DefaultPreview(){
+fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting(name = "Louis Ha")
+        MessageCard(name = "Louis Ha", "Chào mừng các bạn đến với demo jetpack")
     }
 }
